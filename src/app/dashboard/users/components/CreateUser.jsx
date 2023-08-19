168cc8f6fb94cd1b910e6f2fac0e849d25ba24c4
OnlyCrays <cristhian.amaya13@gmail.com>
2023-08-19 10:52:27 -0400

    customer fix

--- src/app/dashboard/users/components/CreateUser.jsx
+++ src/app/dashboard/users/components/CreateUser.jsx
@@ -117,15 +117,15 @@ export default function CreateUser() {
 
 										<div className=" w-48">
 											<label
-												htmlFor="costumerId"
+												htmlFor="customerId"
 												className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
 												Id de Cliente
 											</label>
 											<div className="mt-2">
 												<input
 													onChange={handlerChange}
-													id="costumerId"
-													name="costumerId"
+													id="customerId"
+													name="customerId"
 													type="text"
 													required
 													className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-dark-blue-2 sm:text-sm sm:leading-6 outline-none transition-all duration-500 "
